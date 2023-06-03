2b1a33273f467c7d23258fce2c93e43e1bbe49c7
Andrew Damico <andrew.damico@u.northwestern.edu>
2023-06-03 17:44:19 -0500

    Cleaned up and reformatted code
    # Conflicts:
    #	CLIP_training_Rad.ipynb

--- clip_classes.py
+++ clip_classes.py
@@ -17,37 +17,82 @@ from transformers import TrainingArguments, Trainer
 import torch
 from torch.cuda.amp import autocast
 
-class CLIPDataset(Dataset):
-    def __init__(self, image_paths: list, text: list, mode: str = 'train'):
-        self.image_paths = image_paths
-        self.tokens = tokenizer(text, padding='max_length',
-                                max_length=MAX_LEN, truncation=True)
-
-        if mode == 'train':
-            self.augment = transforms.Compose([
+def get_transforms(mode='train'):
+    
+    '''
+    Performs image augmentation
+    '''
+    
+    if mode == 'train':
+        transforms = torch.nn.Sequential(
+            [
                 #transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
                 transforms.RandomHorizontalFlip(p=0.5),
                 transforms.RandomVerticalFlip(p=0.5),
                 transforms.RandomAutocontrast(),
                 transforms.RandomAdjustSharpness(sharpness_factor=2),
                 transforms.RandomRotation(degrees=(15)),
-                transforms.ToTensor(),
+                #transforms.ToTensor(),
                 transforms.Normalize(mean=MEAN, std=STD)
-            ])
-        elif mode == 'test':
-            self.augment = transforms.Compose([
+            ]
+        )
+        
+    else:
+        transforms = torch.nn.Sequential(
+            [
                 #transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
-                transforms.ToTensor(),
+                #transforms.ToTensor(),
                 transforms.Normalize(mean=MEAN, std=STD)
-            ])
+            ]
+        )
+        
+    return torch.jit.script(transforms)
+    
+
+
+class CLIPDataset(Dataset):
+    def __init__(self, 
+                 image_paths: list, 
+                 text: list, 
+                 #mode: str = 'train'
+                 transformations
+                ):
+        
+        self.image_paths = image_paths
+        self.captions = text #already a list?
+        self.tokens = tokenizer(
+            self.captions, 
+            padding = 'max_length',
+            truncation = True,
+            max_length = MAX_LEN
+        )
+        
+        self.transforms = transformations #new
 
     def __getitem__(self, idx):
         token = self.tokens[idx]
-        return {'input_ids': token.ids, 'attention_mask': token.attention_mask,
-                'pixel_values': self.augment(Image.open(self.image_paths[idx]).convert('RGB'))}
+        
+        item = {
+            key: torch.tensor(values[idx])
+            for key, values in self.tokens.items()
+        }
+        
+        image = open(self.image_paths[idx])
+        image = image.convert('RGB')
+        image = self.transforms(image=image)['image']
+        item['image'] = torch.tensor(image).permute(2, 0, 1).float()
+        item['caption'] = self.captions[idx]
+        
+        return item
+        
+        #return {
+        #    'input_ids': token.ids, 
+        #    'attention_mask': token.attention_mask,
+        #    'pixel_values': self.augment(Image.open(self.image_paths[idx]).convert('RGB'))}
 
     def __len__(self):
-        return len(self.image_paths)
+        #return len(self.image_paths)
+        return len(self.captions)
     
 class CLIPTrainer(Trainer):
     def compute_loss(self, model, inputs, return_outputs=False):
